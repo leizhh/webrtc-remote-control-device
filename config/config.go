a259ac2bfebc355fa1a846582bcd02594ac20ae9
leizhh <1044657730@qq.com>
2020-04-19 16:46:41 +0000

    tidy the code

--- config/config.go
+++ config/config.go
@@ -9,15 +9,20 @@ var (
 		ICEServers: []webrtc.ICEServer{
 			{
 				URLs: []string{"stun:118.89.111.54:3478"},
+			}, {
+				URLs:           []string{"turn:118.89.111.54:3478"},
+				Username:       "leizhh",
+				Credential:     "leizhh",
+				CredentialType: webrtc.ICECredentialTypePassword,
 			},
 		},
 	}
 
-	DeviceId          = ""
-	DefaultServerAddr = ""
+	DeviceId          = "Gateway-51a8da4c"
+	DefaultServerAddr = "118.89.111.54:8080"
 	DefaultAudioSrc   = "audiotestsrc"
-	DefaultVideoSrc   = "videotestsrc"
-	//DefaultVideoSrc = "autovideosrc ! video/x-raw, width=320, height=240 ! videoconvert ! queue"
-	SSHHost = "127.0.0.1"
-	SSHPort = 22
+	//DefaultVideoSrc = "videotestsrc"
+	DefaultVideoSrc = "autovideosrc ! video/x-raw, width=320, height=240 ! videoconvert ! queue"
+	SSHHost         = "127.0.0.1"
+	SSHPort         = 22
 )
